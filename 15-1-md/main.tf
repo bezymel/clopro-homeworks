c866058e31d024677c4f8ffd9292dab663425775
Denis <129361495+bezymel@users.noreply.github.com>
2024-11-22 21:54:41 +0300

    Update main.tf

--- 15-1-md/main.tf
+++ 15-1-md/main.tf
@@ -24,7 +24,6 @@ resource "yandex_vpc_subnet" "public_subnet" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.my_vpc.id
   v4_cidr_blocks = ["192.168.10.0/24"]
-  route_table_id = "enpbfu3cd9smcmo3n55a"  // Замените на свой идентификатор таблицы маршрутов, если это необходимо
 }
 
 resource "yandex_vpc_subnet" "private_subnet" {
@@ -32,34 +31,13 @@ resource "yandex_vpc_subnet" "private_subnet" {
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.my_vpc.id
   v4_cidr_blocks = ["192.168.20.0/24"]
-  route_table_id = "enpbfu3cd9smcmo3n55a"  // Замените на свой идентификатор таблицы маршрутов, если это необходимо
 }
 
-// Создание внешнего IP для публичной ВМ
-resource "yandex_compute_instance" "public_vm" {
-  name = "public-vm"
-
-  resources {
-    cores = 4
-    memory = 4
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.public_subnet.id
-    nat      = false
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd8g2f1p8c2bpk14b4v0"  // Укажите нужный вам образ
-    }
-  }
-}
-
-// Пример NAT-инстанса для приватной ВМ
+// Создание NAT инстанса
 resource "yandex_compute_instance" "nat_instance" {
-  name = "nat-instance"
-
+  name            = "nat-instance"
+  zone            = "ru-central1-a"
+  platform_id     = "standard-v1"
   resources {
     cores = 2
     memory = 2
@@ -67,32 +45,44 @@ resource "yandex_compute_instance" "nat_instance" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.public_subnet.id
+    nat      = true
   }
 
   boot_disk {
     initialize_params {
-      image_id = "fd8g2f1p8c2bpk14b4v0"  // Укажите нужный вам образ
+      image_id = "fd81id4ciatai2csff2u" // Укажите необходимый ID образа
     }
+  }
+
+  // Укажите информацию о авторизации, если необходимо
+  metadata = {
+    ssh-keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo8IE6EQzy6ilrIgbAn+85ccWqC5a7Ol6BgLr9KaHwB8gksrTq0Ur9UMAULaHuK4ZQHeRL5BQRsAjHXMV2tHP+/KGTSBg3D+bAF0XC4y7TUsa9J4/BW2PeV0HQnfK/VXTFEOnnu3pIZl5E4Z5MbzDM3A3aBj>
   }
 }
 
-// Создание приватной ВМ
-resource "yandex_compute_instance" "private_vm" {
-  name = "private-vm"
-
+// Приватная виртуальная машина
+resource "yandex_compute_instance" "private_instance" {
+  name            = "private-instance"
+  zone            = "ru-central1-a"
+  platform_id     = "standard-v1"
   resources {
-    cores  = 4
-    memory = 4
+    cores = 2
+    memory = 2
   }
 
   network_interface {
     subnet_id = yandex_vpc_subnet.private_subnet.id
-    nat       = true // Использование NAT для доступа к интернету
   }
 
   boot_disk {
     initialize_params {
-      image_id = "fd8g2f1p8c2bpk14b4v0"  // Укажите нужный вам образ
+      image_id = "fd81id4ciatai2csff2u" // Укажите необходимый ID образа
     }
   }
+
+  // Укажите информацию о авторизации, если необходимо
+  metadata = {
+    ssh-keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo8IE6EQzy6ilrIgbAn+85ccWqC5a7Ol6BgLr9KaHwB8gksrTq0Ur9UMAULaHuK4ZQHeRL5BQRsAjHXMV2tHP+/KGTSBg3D+bAF0XC4y7TUsa9J4/BW2PeV0HQnfK/VXTFEOnnu3pIZl5E4Z5MbzDM3A3aBj>
+  }
 }
+
