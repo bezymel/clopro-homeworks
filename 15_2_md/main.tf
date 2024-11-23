c4d4cd25a704137463439f27e535f0ede02f37bd
Denis <129361495+bezymel@users.noreply.github.com>
2024-11-23 09:10:04 +0300

    Update main.tf

--- 15_2_md/main.tf
+++ 15_2_md/main.tf
@@ -88,7 +88,12 @@ resource "yandex_compute_instance" "private_instance" {
 
 // Создание бакета Object Storage
 resource "yandex_storage_bucket" "my_bucket" {
-  bucket = "denis_1997-04-10" # Замените на ваше имя и дату.
+  bucket = "denis-10-04-1997" # Замените на ваше имя и дату. Пример: "ivanov-2024-04-01"
+  
+  access_control {
+    role     = "viewer"
+    principal = "allUsers"
+  }
 }
 
 // Загрузка файла в бакет
@@ -100,43 +105,45 @@ resource "yandex_storage_object" "my_image" {
 
 // Группа ВМ с заданием для LAMP
 resource "yandex_compute_instance_group" "lamp_group" {
-  name = "lamp-instance-group"
-  
-  allocation_policy {
-    zones = ["ru-central1-a"]
+  name        = "lamp-instance-group"
+  zone        = "ru-central1-a"
+  instances   = 3 // Количество инстансов
+  platform_id = "standard-v1"
 
-    # Укажите экземпляр в группе
-    instances {
-      platform_id = "standard-v1"
-      resources {
-        cores   = 2
-        memory  = 2
-      }
+  template {
+    resources {
+      cores   = 2
+      memory  = 2
+    }
 
-      boot_disk {
-        initialize_params {
-          image_id = "fd81id4ciatai2csff2u" // Укажите ID образа LAMP
-        }
-      }
+    network_interface {
+      subnet_id = yandex_vpc_subnet.public_subnet.id
+    }
 
-      network_interface {
-        subnet_id = yandex_vpc_subnet.public_subnet.id
+    boot_disk {
+      initialize_params {
+        image_id = "fd81id4ciatai2csff2u" // Укажите ID образа LAMP
       }
+    }
 
-      metadata = {
-        ssh-keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo8IE6EQzy6ilrIgbAn+85ccWqC5a7Ol6BgLr9KaHwB8gksrTq0Ur9UMAULaHuK4ZQHeRL5BQRsAjHXMV2tHP+/KGTSBg3D+bAF0XC4y7TUsa9J4/BW2PeV0HQnfK/VXTFEOnnu3pIZl5E4Z5MbzDM3A3aBj57badbetWAZLY/Kwq1IE9GWkkXEhUX8j6ymp6vlcd+7sAo5OEVv+HP8IBdO6ilwwbNreTH8UOyscBq2nm29d5jcRlvqxvripaQdvtFli8V6xRxS1B/7TQvoxL5u3GOigOMSC27km2g4fqIKlZ/LKX0yDGY3G/FHmJSYCt0Zt/WjmoC2yxUSYgrG7wP your_email@example.com"
-      }
+    metadata = {
+      ssh-keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCo8IE6EQzy6ilrIgbAn+85ccWqC5a7Ol6BgLr9KaHwB8gksrTq0Ur9UMAULaHuK4ZQHeRL5BQRsAjHXMV2tHP+/KGTSBg3D+bAF0XC4y7TUsa9J4/BW2PeV0HQnfK/VXTFEOnnu3pIZl5E4Z5MbzDM3A3aBj57badbetWAZLY/Kwq1IE9GWkkXEhUX8j6ymp6vlcd+7sAo5OEVv+HP8IBdO6ilwwbNreTH8UOyscBq2nm29d5jcRlvqxvripaQdvtFli8V6xRxS1B/7TQvoxL5u3GOigOMSC27km2g4fqIKlZ/LKX0yDGY3G/FHmJSYCt0Zt/WjmoC2yxUSYgrG7wP your_email@example.com"
     }
   }
 
-  scale_policy {
-    approximate_scaling_unit = 2
-    min_size                = 1
-    max_size                = 5
+  // Добавление healthcheck
+  healthcheck {
+    path      = "/"
+    port      = 80
+    protocol  = "HTTP"
+    interval  = 30
+    timeout   = 5
+    healthy_threshold = 2
+    unhealthy_threshold = 3
   }
 
-  deploy_policy {
-    max_unavailable = 1
+  allocation_policy {
+    zones = ["ru-central1-a"]
   }
 }
 
